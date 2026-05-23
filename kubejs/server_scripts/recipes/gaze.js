95373aa101f969aa417b12d82c055b56bdcc4b2b
Rynxotic <ryancooke71@gmail.com>
2026-05-23 12:47:19 +0100

    Removed commented items.

--- kubejs/server_scripts/recipes/gaze.js
+++ kubejs/server_scripts/recipes/gaze.js
@@ -6,12 +6,3 @@ ServerEvents.recipes((event) => {
     output: ["gaze:rune_of_out_of_phase"],
 });
 });
-//ServerEvents.recipes((event) => {
-  // RECIPES
-//  event.recipes.createSequencedAssembly([
-//    Item.of('gaze:run_of_out_of_phase')
-//  ], 'minecraft:nether_star', [
-//    event.recipes.createDeploying('gaze:run_of_out_of_phase', ['minecraft:nether_star', 'minecraft:nether_star']),
-//    event.recipes.createPressing('gaze:run_of_out_of_phase', 'gaze:run_of_out_of_phase'),
-//   event.recipes.createCutting('gaze:run_of_out_of_phase', 'gaze:run_of_out_of_phase'),
-//    event.recipes.createPressing('gaze:run_of_out_of_phase', 'gaze:run_of_out_of_phase')
